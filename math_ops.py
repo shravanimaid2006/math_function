7899eb3fa1afc749e14e157460f3a83e622c2c1e
Aayush <aayushpednekar0309@gmail.com>
2025-12-02 15:10:02 +0530

    Modified the math function file

--- math_ops.py
+++ math_ops.py
@@ -1,2 +1,13 @@
 def add(a,b):
     return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b==0:
+     return "Division by zero error"
+    return a/b
